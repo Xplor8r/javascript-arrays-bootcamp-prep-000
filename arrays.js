cb8cd491715464d28850eecbffb424da0bc1e03c
Michael Pieschel <sixtyhectares@yahoo.com>
2018-07-19 03:46:49 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -15,7 +15,7 @@ function destructivelyAddElementToEndOfArray (chocolateBars, foo) {
   chocolateBars.push("foo");
   return chocolateBars;
 }
-function accessElementInArray(chocolateBars, 0){
+function accessElementInArray(chocolateBars, index){
   chocolateBars[0];
   return chocolateBars;
 }
